49939e2051a120eaef6554f60e3bbcddb89ed816
Amendez227 <amendez14000@gmail.com>
2025-11-14 12:21:38 -0600

    Some more react progress

--- Front _End/OnlineStoreReact/src/message.tsx
+++ Front _End/OnlineStoreReact/src/message.tsx
@@ -1,6 +1,6 @@
 
 function Message() {
-    return <h1> Jaosn Hates Water and two eggs make a duck</h1>;
+    return <h1> Jason Hates Water and two eggs make a duck</h1>;
 }
 
 export default Message;
