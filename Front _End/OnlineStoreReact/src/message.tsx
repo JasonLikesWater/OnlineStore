482254706e93093ac6666fc3525f071efed11ab6
Amendez227 <amendez14000@gmail.com>
2025-11-17 11:27:44 -0600

    Added a non functional navigation bar

--- Front _End/OnlineStoreReact/src/message.tsx
+++ Front _End/OnlineStoreReact/src/message.tsx
@@ -1,5 +1,16 @@
 function Message() {
-  return <h1> Start of the website</h1>;
+  return (
+    <>
+      {" "}
+      <figure className="text-center">
+        <blockquote className="blockquote">
+          <p>Your One-Stop Shop For Movies.</p>
+          <p>Discover your next film...</p>
+        </blockquote>
+      </figure>
+      ;
+    </>
+  );
 }
 
 export default Message;
