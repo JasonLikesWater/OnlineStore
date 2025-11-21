f70e48f49cbcc26c5d4c8e156d0abafa962ac9a3
NicholasRathmann <nickrathmann103@gmail.com>
2025-11-21 14:54:12 -0600

    simplify server start, add readme

--- Back_End/server.js
+++ Back_End/server.js
@@ -10,6 +10,11 @@ app.get('/api', (req, res) => {
   res.json({ message: "Backend is working!" });
 });
 
+// temp message
+app.get('/', (req, res) => {
+  res.send('there is a test route, it is /api');
+})
+
 app.listen(5000, () => {
   console.log("Server running on http://localhost:5000");
 });
