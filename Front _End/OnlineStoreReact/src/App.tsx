f7483c10b3ac8ddea82772f18d3ab65a7156e932
Howraa-777 <hal-robaie2@huskers.unl.edu>
2025-11-24 09:10:15 -0600

    Added checkout page and styling

--- Front _End/OnlineStoreReact/src/App.tsx
+++ Front _End/OnlineStoreReact/src/App.tsx
@@ -1,10 +1,12 @@
 import Message from "./message";
 import NavBar from "./Components/Layout/Header/navBar.tsx";
+import CheckoutPage from "./Components/Pages/checkoutPage";
 
 function App() {
   return (
     <div>
       <NavBar />
+      <CheckoutPage />
       <Message />
     </div>
   );
