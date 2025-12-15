72c00b5116a18f6d457b7eeaaf46d3d4ac4fc47f
JasonLikesWater <116331386+JasonLikesWater@users.noreply.github.com>
2025-12-15 12:35:44 -0600

    Fixed New Function in Interface

--- Back_End_C/Interfaces/IMovieRepository.cs
+++ Back_End_C/Interfaces/IMovieRepository.cs
@@ -6,4 +6,6 @@ public interface IMovieRepository
     IEnumerable<OnlineStore.Models.Genre> GetMovieGenres(int movieId);
     IEnumerable<Person> GetMoviePeople(int movieId);
     IEnumerable<Review> GetMovieReviews(int movieId);
-    IEnumerable<OnlineStore.Models.MovieDetails> GetMovieEverything(int movieId);}
+    IEnumerable<OnlineStore.Models.MovieDetails> GetMovieEverything(int movieId);
+    IEnumerable<Movie> GetMoviesByTitleLike(string titleFragment);
+    }
